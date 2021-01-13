620db60ca01306932fcc48cb54f118d72214e64d
veronica guerrini <veronica_guerrini@hotmail.it>
2021-01-13 16:19:43 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,4 +3,7 @@ def display_rainbow(arr)
   puts "R: #{arr[0]}, O: #{arr[1]}, Y: #{arr[2]}, G: #{arr[3]}, B: #{arr[4]}, I: #{arr[5]}, V: #{arr[6]}"
 end
 def display_rainbow(color)
-  puts "R: #{color[0]}, O: #"{color[1]}, Y: #{color[2]}, G #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]
+  puts "R: #{color[0]}, O: #"{color[1]}, Y: #{color[2]}, G #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}"
+end
+
+  
